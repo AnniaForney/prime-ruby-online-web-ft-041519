a600783080490eee7b094844ded9294a0b5e84d4
Annia Forney <anniaforney@gmail.com>
2019-04-20 22:39:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,6 @@
 # Add  code here!
-require 'benchmark'
-
-2
-3
-4
-5
-require 'benchmark'
+ require'benchmark'
 require 'bigdecimal/math'
 
 # calculate pi to 10k digits
-puts Benchmark.measure
+puts Benchmark.measure { BigMath.PI(10_000) }
